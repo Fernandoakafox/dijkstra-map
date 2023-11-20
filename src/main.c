3baedfac7b8bd002bf23b8565314cf766c1b1c78
Gabriel Werner Kuhn <gkuhn@imply.com>
2023-11-20 20:44:41 -0300

    fix [main / GraphTools]: fix
    - fix arros de compilação

--- src/main.c
+++ src/main.c
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//ATENÇÃO -> O que esta comentado em main, esta comentado pois ao rodar da bug, descobrir o bug
+//ATENÇÃO -> O que esta comentado em main, esta comentado pois ao rodar da bug
+// TODO descobrir o bug
 int main (){
     struct GRAFO *grafo = criaGrafo(4);
     adicionarVertice(grafo, 0, "B1", 871, 351);
-    adicionarVertice(grafo, 1, "0", 871, 479);
-    adicionarVertice(grafo, 2, "0", 799, 479);  // 0 pode significar encruzilhada ou algo do tipo
+    adicionarVertice(grafo, 1, "B0", 871, 479);
+    adicionarVertice(grafo, 2, "B0", 799, 479);  // 0 pode significar encruzilhada ou algo do tipo
     adicionarVertice(grafo, 3, "B2", 799, 536);
 
     adicionarAresta(grafo, 0, 1);
@@ -18,7 +19,7 @@ int main (){
 
 
     struct Pilha *dijkstraRoute = dijkstra(grafo, 0, 3);
-    //printf("TOPO: %i\n", dijkstraRoute->topo->vertice); O bug parece ser com a pilha
+    //printf("TOPO: %i\n", dijkstraRoute->topo->vertice); // O bug parece ser com a pilha
 
     plot_route(&dijkstraRoute, grafo);
 }
