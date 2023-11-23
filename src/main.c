e705cc4e8409aee6078d239f6bfc05efad51b29e
Fernandoakafox <fernando_contas@yahoo.com.br>
2023-11-23 17:03:22 -0300

    fix[main]
    - Reestruturação da main de modo a se adequar ao novo metodo de adicionar vetices e arestas, através da utilização da função importaGrafo.

--- src/main.c
+++ src/main.c
@@ -7,19 +7,13 @@
 //ATENÇÃO -> O que esta comentado em main, esta comentado pois ao rodar da bug
 // TODO descobrir o bug
 int main (){
-    struct GRAFO *grafo = criaGrafo(4);
-    adicionarVertice(grafo, 0, "B1", 871, 351);
-    adicionarVertice(grafo, 1, "B0", 871, 479);
-    adicionarVertice(grafo, 2, "B0", 799, 479);  // 0 pode significar encruzilhada ou algo do tipo
-    adicionarVertice(grafo, 3, "B2", 799, 536);
+    struct GRAFO *grafo = criaGrafo(38); //temos vertice 0, logo se o nosso ultimo vertice é o 37, temos no total 38 vertices
 
-    adicionarAresta(grafo, 0, 1);
-    adicionarAresta(grafo, 1, 2);
-    adicionarAresta(grafo, 2, 3); 
+    importaGrafo(grafo);
 
-
-    struct Pilha *dijkstraRoute = dijkstra(grafo, 0, 3);
-    printf("TOPO: %i\n", dijkstraRoute->topo->vertice); // O bug parece ser com a pilha
-
-    plot_route(&dijkstraRoute, grafo);
+        
+    //struct Pilha *dijkstraRoute = dijkstra(grafo, 5, 35);
+    //printf("TOPO: %i\n", dijkstraRoute->topo->vertice); // O bug parece ser com a pilha
+    
+    //plot_route(&dijkstraRoute, grafo);
 }
