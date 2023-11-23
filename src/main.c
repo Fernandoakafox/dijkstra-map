ce7d3f51bc70aa74db9a5807c097f93ef4a98775
Fernandoakafox <fernando_contas@yahoo.com.br>
2023-11-23 19:01:39 -0300

    fix[main] - Comentarios modificados

--- src/main.c
+++ src/main.c
@@ -4,16 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//ATENÇÃO -> O que esta comentado em main, esta comentado pois ao rodar da bug
-// TODO descobrir o bug
 int main (){
     struct GRAFO *grafo = criaGrafo(38); //temos vertice 0, logo se o nosso ultimo vertice é o 37, temos no total 38 vertices
 
     importaGrafo(grafo);
-
         
-    //struct Pilha *dijkstraRoute = dijkstra(grafo, 5, 35);
-    //printf("TOPO: %i\n", dijkstraRoute->topo->vertice); // O bug parece ser com a pilha
+    //struct Pilha *dijkstraRoute = dijkstra(grafo, 5, 17);
+    //printf("TOPO: %i\n", dijkstraRoute->topo->vertice);
     
     //plot_route(&dijkstraRoute, grafo);
 }
